96c9a2743dfff25612697c78517cca9585e70d13
tharun <godasutharun143@gmail.com>
2026-02-02 17:54:13 +0530

    first commit on spellcheck

---
+++ src/SpellCheck.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+export default function SpellCheck(){
+
+    const [text, setText] = useState(" ");
+    const [suggestText, setSuggestText] = useState(" ");
+
+    const handleSpellCheck=(e)=>{
+        const inputText = e.target.value;
+         setText(inputText);
+
+         const words = text.split(" ");
+         const correctWords = words.map((word)=>{
+            const correctWord = customDictionary[word.toLowerCase()];
+            return correctWord || word;
+         });
+
+        //  const correctText = correctWords.join(" ");
+
+         const firstCorrection = correctWords.find(
+            (word, index)=> word !== words[index]
+         );
+         setSuggestText(firstCorrection || "");
+    }
+
+const customDictionary = {
+  teh: "the",
+  wrok: "work",
+  fot: "for",
+  exampl: "example",
+};
+
+    return(
+        <div>
+        <h1>Spell Check and auto-Correction</h1>
+        <textarea placeholder="Enter text..." onChange={handleSpellCheck} rows={5} cols={40}></textarea>
+
+        {suggestText && (
+            <p>Did you mean: <strong>{suggestText}</strong></p>
+        )}
+        </div>
+    )
+}
